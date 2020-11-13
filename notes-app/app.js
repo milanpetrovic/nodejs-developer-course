26ed320d4d7b8ac60e03b29d3ff05b4d8353edec
milanpetrovic <milanmushin@gmail.com>
2020-11-13 14:04:23 +0100

    Printing in color

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,15 @@
-const validator = require('validator');
+// const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes');
 
-console.log(getNotes());
+const log = console.log;
 
-console.log(validator.isEmail('milanmushin@gmail.com'));
+const successLog = chalk.green.inverse;
+const errorLog = chalk.white.bgRed.bold;
+const warningLog = chalk.black.bgHex('#FF8800');
+
+log(getNotes());
+
+log(successLog('This is a success message!'));
+log(errorLog('This is a big fat error!'));
+log(warningLog('I warn you, don\'t you dare to stop learning MERN!'));
